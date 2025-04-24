9f74d0413b51d4b42f88eae69de1a9632120bfb9
akshu <in.akr.2000@gmail.com>
2025-04-25 00:43:28 +0530

    Resume improved

--- src/pages/ResumePage/ResumePage.jsx
+++ src/pages/ResumePage/ResumePage.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import resume from "../../assets/Akshansh_Resume.pdf";
 import styles from "./ResumePage.module.scss";
+import Navbar from "../../components/Navbar/Navbar";
 
 const ResumeViewer = () => {
   return (
     <div className={styles.container}>
+      {/* <Navbar /> */}
       <iframe src={resume} title="Resume" className={styles.dimensions} />
     </div>
   );
